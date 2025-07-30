66cac0ad7633376a298b60e4c7ae57741a8d1774
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-07-30 12:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function fizzBuzz(n){
+	let res=[];
+	for(let i=1;i<=n;i++){
+		if(i%3===0 && i%5===0){
+			res.push("FizzBuzz");
+		}else if(i%3===0){
+			res.push("Fizz");
+		}else if(i%5===0){
+			res.push("Buzz");
+		}else{
+			res.push(i);
+		}
+	}
+	return res;
+}
+const n=prompt("enter a number");
+alert(fizzBuzz(n));
